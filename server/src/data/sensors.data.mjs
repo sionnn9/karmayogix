bc4e6c1c5d4fa18d01fa7b0a4a079adc98ff9152
Nidhi Anna Sequeira <sequeiranidhianna@gmail.com>
2026-02-07 15:43:27 +0530

    fix: sensors.data.mjs

--- server/src/data/sensors.data.mjs
+++ server/src/data/sensors.data.mjs
@@ -20,3 +20,12 @@ module.exports = sensors;
 
 // This object lives in RAM
 // When Arduino sends data, we update this object
+
+/* Arduino sends an HTTP POST request like this:
+    POST http://<server-ip>:3000/api/sensors/data
+
+Body:    {
+  "sensorId": "S1",
+  "waterLevel": 78
+}
+*/
